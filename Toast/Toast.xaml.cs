b898ca16d84b1330d40c8540044abe4efecb6ab8
Marc Rousavy <Marc Rousavy>
2016-12-28 17:04:14 +0100

    Added Demo

--- Toast/Toast.xaml.cs
+++ Toast/Toast.xaml.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Toast {
     /// <summary>
     /// Interaction logic for Toast.xaml
     /// </summary>
     public partial class Toast : UserControl {
-        public string Message { get; set; } = "No Message set.";
+
+
+        //User-Changeable Values
+        public string Message { get; set; }
         public TimeSpan Duration { get; set; } = TimeSpan.FromSeconds(2);
+        public TimeSpan DurationAnimation { set; get; } = TimeSpan.FromMilliseconds(300);
+        public ToastDuration DurationToast {
+            set {
+
+                _internalDurationToast = value;
+            }
+            get {
+                return _internalDurationToast;
+            }
+        }
+
+        public new object Content {
+            set {
+                Message = value as string;
+            }
+            get {
+                return Message;
+            }
+        }
+
+
+        public enum ToastDuration { Short, Medium, Long }
+        private ToastDuration _internalDurationToast;
+
 
 
         public Toast() {
@@ -16,7 +45,16 @@ namespace Toast {
 
 
         public void Show(string message, TimeSpan duration) {
+            ToastContent.Text = message;
 
+            DoubleAnimation anim = new DoubleAnimation(this.Opacity, 1d, this.DurationAnimation);
+
+            anim.Completed += async delegate {
+                await Task.Delay(this.Duration);
+                Hide();
+            };
+
+            this.BeginAnimation(UserControl.OpacityProperty, anim);
         }
 
 
@@ -30,6 +68,36 @@ namespace Toast {
 
         public void Show(TimeSpan duration) {
             Show(this.Message, duration);
+        }
+
+        public void Show(ToastDuration duration) {
+            Show(this.Message, ToastDurationToTimeSpan(duration));
+        }
+
+
+        public void Hide() {
+            DoubleAnimation anim = new DoubleAnimation(this.Opacity, 0d, this.DurationAnimation);
+            this.BeginAnimation(UserControl.OpacityProperty, anim);
+        }
+
+
+        private TimeSpan ToastDurationToTimeSpan(ToastDuration tduration) {
+            TimeSpan ret = Duration;
+
+            switch(tduration) {
+                case ToastDuration.Short:
+                    ret = TimeSpan.FromSeconds(2);
+                    break;
+                case ToastDuration.Medium:
+                    ret = TimeSpan.FromSeconds(5);
+                    break;
+                case ToastDuration.Long:
+                    ret = TimeSpan.FromSeconds(10);
+                    break;
+            }
+
+
+            return ret;
         }
     }
 }
---
+++ ToastDemo/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToastDemo {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void ShowToastClick(object sender, RoutedEventArgs e) {
+            InitToast();
+            myToast.Show();
+        }
+
+        private void HideToastClick(object sender, RoutedEventArgs e) {
+            myToast.Hide();
+        }
+
+        private void InitToast() {
+            myToast.Content = ToastNameBox.Text;
+
+            var checkedButton = RadioButtonPanel.Children.OfType<RadioButton>()
+                                      .FirstOrDefault(r => (bool)(r.IsChecked));
+
+            myToast.DurationToast = (Toast.Toast.ToastDuration)Enum.Parse(myToast.DurationToast.GetType(), checkedButton.Content as string);
+        }
+    }
+}
