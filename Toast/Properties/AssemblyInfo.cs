125989e2894bff54c67f1730108e24b055a55b9c
Marc Rousavy <Marc Rousavy>
2016-12-28 15:17:43 +0100

    Added default Framework

--- Toast/Properties/AssemblyInfo.cs
+++ Toast/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Toast")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Android Toast for WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mrousavy")]
 [assembly: AssemblyProduct("Toast")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("mrousavy")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
---
+++ Toast/Toast.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Toast {
+    /// <summary>
+    /// Interaction logic for Toast.xaml
+    /// </summary>
+    public partial class Toast : UserControl {
+        public string Message { get; set; } = "No Message set.";
+        public TimeSpan Duration { get; set; } = TimeSpan.FromSeconds(2);
+
+
+        public Toast() {
+            InitializeComponent();
+        }
+
+
+        public void Show(string message, TimeSpan duration) {
+
+        }
+
+
+        public void Show() {
+            Show(this.Message, this.Duration);
+        }
+
+        public void Show(string message) {
+            Show(message, this.Duration);
+        }
+
+        public void Show(TimeSpan duration) {
+            Show(this.Message, duration);
+        }
+    }
+}
